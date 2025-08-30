2215915ea326d17267e8f06f18ede45a328ce712
Dylan Page <145734049+ConsumeZaDusto@users.noreply.github.com>
2025-08-30 12:46:48 -0700

    Medusa sight is a work in progress for line-of-sight detection. Added the allowed movement in EnemyMovement if there's a wall in between the player and enemy. -DP

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,18 +4,43 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
-    private NavMeshAgent navMeshAgent;
+    public LayerMask obstacleLayerMask = -1; //when layers block line of sight
+    private NavMeshAgent enemyMeshAgent;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        enemyMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
+
+    //Please find a better way to do make the update loop that involves setting the speed to zero or using isStopped on the enemyMeshAgent.
     void Update()
     {
-        if (player != null) {
-            navMeshAgent.SetDestination(player.position);
+        if (player != null && !CanSeePlayer())
+        {
+            enemyMeshAgent.SetDestination(player.position);
         }
+        else
+        {
+            enemyMeshAgent.SetDestination(transform.position);
+        }
+    }
+
+    bool CanSeePlayer()
+    {
+        Vector3 directionToPlayer = (player.position - transform.position).normalized;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        //cast ray from enemy to player
+        if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hitInfo, distanceToPlayer, obstacleLayerMask))
+        {
+            //return truth of if the hit is the player
+            return hitInfo.transform == player;
+        }
+
+        // if no obstacles hit within player distance, the player can be seen
+        return true;
     }
 }
